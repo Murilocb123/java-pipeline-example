8ff958ff22270d1938a769cdbd14243ba7f76f07
Murilo <murilocb1276@gmail.xom>
2025-02-18 21:14:07 -0300

    feature: files

---
+++ src/test/java/com/murilocb123/service_example/ServiceExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilocb123.service_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
